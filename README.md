1168354e6cd1dcc2c670ed1d3ef0f2b39cc5a8fc
Panos Zafeiropoulos <zzpzaf@gmail.com>
2024-01-24 12:23:11 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+
+
+
+
+# *** Note: *** 
+
+This repo is a demo repo using Angular/Angular material versions 15.x with the @angular-material-components/datetime-picker version 15.0.0. The datetimepicker theme inherited OK from the angular material theme and works as expected. However, till the time this repo was created, there was no any updated for the [@angular-material-components/datetime-picker](https://www.npmjs.com/package/@angular-material-components/datetime-picker), that solves the “transparency” issue with Angular/Angular material versions ^16.x. Actually, there were no updates since 6 months ago.
+
+The repo has been created on Jan. 24, 2024.
+
+<img src="./src/assets/GIF1.gif" alt="@angular-material-components/datetime-picker version 15.0.0" title="@angular-material-components/datetime-picker version 15.0.0" width="450"/>
+
+---
+
+
+#### This project has been created via the 'ngcr.sh' tool.
+#### Get it at: https://github.com/zzpzaf/bash-ngrc
+
+(C) 2024 Panos Zafeiropoulos
+
+License: MIT
+
+
---
+++ src/app/material.module.ts
@@ -0,0 +1,40 @@
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {
+  NgxMatDatetimePickerModule,
+  NgxMatNativeDateModule,
+} from '@angular-material-components/datetime-picker';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    NgxMatDatetimePickerModule,
+    NgxMatNativeDateModule,
+
+  ],
+  exports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    NgxMatDatetimePickerModule,
+    NgxMatNativeDateModule,
+  ],
+})
+export class MaterialModule {}
